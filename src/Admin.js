9c24af6d823b15a8c85ae26d8243763a2207e6a9
Ridheim <rudgary@gmail.com>
2020-06-02 01:31:13 +0300

    admin page for pizzas menu is ready. Now all data can be changed and be add using admin page

--- src/Admin.js
+++ src/Admin.js
@@ -3,6 +3,29 @@ import './Auth'
 
 import { pizzas } from './Menu'
 
+//
+//Class to close modals after submiting the form, clearing the fields and refreshing the page. Need to pass modal id in a string! format and id of the form to close
+class closeForms{
+  constructor(modal, form){
+    this.modal = document.querySelector(`#${modal}`);
+    this.form = form;
+  }
+  modalClose(){
+    M.Modal.getInstance(this.modal).close();
+    this.form.reset();
+    location.reload();
+  }
+}
+
+//
+//firebase getting true and false as a string, this func transforms string into the boolean.
+const boolcheck = (checkVal) => {
+  if(checkVal == 'true'){
+    return true
+  } else {
+    return false
+  }
+}
 
 // 
 //--get pizzas from DB and rendering it to the screen in table form
@@ -21,11 +44,9 @@ pizzas.get()
 const RenderPizzas = (pizza) => {
     let html = `
       <tr pizza-id=${pizza.id}>
+        <td><i href="#modal_edit" class="material-icons delete green-text darken-1-text modal-trigger">edit</i></td>
         <td>${pizza.data().title}</td>
         <td>${pizza.data().price}</td>
-        <td>${pizza.data().vegan}</td>
-        <td>${pizza.data().img}</td>
-        <td>${pizza.data().description}</td>
         <td><i class="material-icons delete red-text darken-1-text">delete</i></td>
       </tr>
     `
@@ -33,23 +54,104 @@ const RenderPizzas = (pizza) => {
 }
 
 //
-//--getting data from input fields and adding them to a DB
-const form = document.querySelector('#addPizza');
+//function to upload image to the storage. Need to pass id of the input field with file location in string! format and id of the img tag to render uploaded image
+const uploading = (input, output) => {
+  const file = document.querySelector(`#${input}`).files[0];
+  if(file){
+    const name = Math.floor(Math.random() * 999) + file.name;
+    const metadata = {
+      contentType:file.type
+    }
+    storage.ref().child(name).put(file, metadata)
+    .then(snapshot => {
+      snapshot.ref.getDownloadURL().then(url => {
+        output.src = url;
+      })
+    }).catch(err => console.log(err.message))
+  } else {
+    console.log('You need to choose a file first')
+  }
+}
 
-form.addEventListener('submit', (e)=> {
+//
+//--Creating pizza. Getting data from the input fields and adding them to a DB
+const addform = document.querySelector('#addPizza');
+const uploadbtn = document.querySelector('#upload-btn');
+const img = document.querySelector('#img-create');
+
+//uploading img to the storage
+uploadbtn.addEventListener('click', e => {
+  e.preventDefault();
+  uploading('file-create', img);
+});
+
+//adding text data to the db
+addform.addEventListener('submit', (e)=> {
   e.preventDefault();
 
-  db.collection('pizzas').add({
-    title: form.title.value,
-    price: parseInt(form.price.value),
-    vegan: Boolean(form.vegan.value),
-    img: form.img.value,
-    description: form.description.value
+  pizzas.add({
+    title: addform.title.value,
+    price: parseInt(addform.price.value),
+    vegan: boolcheck(addform.vegan.value),
+    img: img.src,
+    description: addform.description.value
   }).then(() => {
-    const modal = document.querySelector('#modal-create');
-    M.Modal.getInstance(modal).close();
-    form.reset();
-    location.reload();
+    const close = new closeForms('modal_create', addform);
+    close.modalClose();
+  }).catch(err => {
+    console.log(err.message);
+  })
+})
+
+//
+//--editing existing pizza
+const pizzaList = document.querySelector('#pizzaList')
+const editform = document.querySelector('#editPizza');
+const imgEdit = document.querySelector('#img-edit');
+
+//getting data from db an inserting existed data into the form
+pizzaList.addEventListener('click', e => {
+  if(e.target.tagName === 'I' && e.target.textContent == 'edit'){
+    const posID = e.target.parentElement.parentElement.getAttribute('pizza-id');
+    pizzas.doc(posID)
+          .get()
+          .then((pizza) => {
+      editform['disabled'].value = posID;
+      editform['edit-title'].value = pizza.data().title;
+      editform['edit-price'].value = pizza.data().price;
+      editform['vegan-edit'].value = pizza.data().vegan;
+      editform['edit-description'].value = pizza.data().description;
+      imgEdit.src = pizza.data().img;
+      M.updateTextFields();
+      M.textareaAutoResize(editform['edit-description']);
+    }).catch(err => {
+      console.log(err.message);
+    });
+  }
+})
+
+const uploadbtnEdit = document.querySelector('#upload-btn-edit');
+
+//uploading img to the storage
+uploadbtnEdit.addEventListener('click', e => {
+  e.preventDefault();
+  uploading('file-edit', imgEdit);
+});
+
+//submiting editied data into the db
+editform.addEventListener('submit', (e)=> {
+  e.preventDefault();
+
+  const posID = editform['disabled'].value;
+  pizzas.doc(posID).set({
+    title: editform['edit-title'].value,
+    price: parseInt(editform['edit-price'].value),
+    vegan: boolcheck(editform['vegan-edit'].value),
+    img: imgEdit.src,
+    description: editform['edit-description'].value
+  }).then(() => {
+    const close = new closeForms('modal_edit', editform);
+    close.modalClose();
   }).catch(err => {
     console.log(err.message);
   })
@@ -57,11 +159,10 @@ form.addEventListener('submit', (e)=> {
 
 //
 //--Deleting pizza from DB
-const pizzaList = document.querySelector('#pizzaList')
 pizzaList.addEventListener('click', e => {
-  if(e.target.tagName === 'I'){
+  if(e.target.tagName === 'I' && e.target.textContent == 'delete'){
     const posID = e.target.parentElement.parentElement.getAttribute('pizza-id');
-    pizzas.doc(posID).delete().then(function() {
+    pizzas.doc(posID).delete().then(() => {
       location.reload();
     }).catch(function(error) {
         console.error("Error removing document: ", error);
